f284490421fc83c1806f92dec34ac3c726180ebe
GiovanaGuedesSilva <giovanagwynguedes@gmail.com>
2026-03-05 18:52:32 -0300

    fix: load dotenv and close DB/Redis on graceful shutdown

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
+import 'dotenv/config';
 import { startServer } from './app.js';
+import { closeDatabase } from './src/config/database.js';
+import { closeRedis } from './src/config/redis.js';
 
 async function bootstrap() {
 	try {
 		const server = await startServer();
 		console.log('Server started successfully');
 
-		process.on('SIGTERM', () => {
-			console.log('SIGTERM signal received: closing HTTP server');
-			server.close(() => {
+		const gracefulShutdown = (signal) => {
+			console.log(`${signal} signal received: closing HTTP server`);
+			server.close(async () => {
+				await closeDatabase();
+				await closeRedis();
 				console.log('HTTP server closed');
 				process.exit(0);
 			});
-		});
+		};
 
-		process.on('SIGINT', () => {
-			console.log('\nSIGINT signal received: closing HTTP server');
-			server.close(() => {
-				console.log('HTTP server closed');
-				process.exit(0);
-			});
-		});
+		process.on('SIGTERM', () => gracefulShutdown('SIGTERM'));
+		process.on('SIGINT', () => gracefulShutdown('SIGINT'));
 	} catch (err) {
 		console.error('Error starting server:', err);
 		process.exit(1);
